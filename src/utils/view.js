e114607991240b464d310b81ddae6df60ddb46d4
vlach1989 <vlach1989@gmail.com>
2020-06-05 07:51:58 +0200

    Comment view utils

--- src/utils/view.js
+++ src/utils/view.js
@@ -1,3 +1,10 @@
+/**
+ * Convert Web World Wind range to Panther boxRange
+ * @param range {WorldWind.Navigator.range}
+ * @param width {number} map width
+ * @param height {number} map height
+ * @return {number|*} Panther view box range
+ */
 function getBoxRangeFromWorldWindRange(range, width, height) {
     if (width && width >= height) {
         return Math.round(range * height/width);
@@ -6,6 +13,13 @@ function getBoxRangeFromWorldWindRange(range, width, height) {
     }
 }
 
+/**
+ * Convert Panther boxRange to Web World Wind range
+ * @param boxRange {number} Panther view boxRange
+ * @param width {number} map width
+ * @param height {number} map height
+ * @return {number|*} WorldWind navigator range
+ */
 function getWorldWindRangeFromBoxRange(boxRange, width, height) {
     if (width && width >= height) {
         return Math.round(boxRange * width/height);
