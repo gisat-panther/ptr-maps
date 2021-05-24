563ecee8cdd9161def563fe53dc5acf263b9d746
Mariana Kecova <mariana.kecova@gisat.cz>
2021-05-24 13:15:03 +0200

    updated layer's thumbnails

--- src/controls/SimpleLayersControl/images.js
+++ src/controls/SimpleLayersControl/images.js
@@ -1,29 +1,33 @@
-
-import cartoDB_DarkMatter from "./img/CartoDB_DarkMatter.png"
-import cartoDB_VoyagerNoLabels from "./img/CartoDB_VoyagerNoLabels.png"
-import esri_WorldGrayCanvas from "./img/Esri_WorldGrayCanvas.png"
-import esri_WorldImagery from "./img/Esri_WorldImagery.png"
-import esri_WorldTopoMap from "./img/Esri_WorldTopoMap.png"
-import noPreview from "./img/noPreview.png"
+import bing_Aerial from './img/Bing_Aerial.png';
+import cartoDB_DarkMatter from './img/CartoDB_DarkMatter.png';
+import cartoDB_VoyagerNoLabels from './img/CartoDB_VoyagerNoLabels.png';
+import esri_WorldGrayCanvas from './img/Esri_WorldGrayCanvas.png';
+import esri_WorldImagery from './img/Esri_WorldImagery.png';
+import esri_WorldTopoMap from './img/Esri_WorldTopoMap.png';
+import noPreview from './img/noPreview.png';
+import openStreetMap_Mapnik from './img/OpenStreetMap_Mapnik.png';
+import wikimedia from './img/wikimedia.png';
 
 // to be deleted (void images)
-import bingAerial from "./img/bingAerial.png"
-import cartoBaseDarkLabeled from "./img/cartoBaseDarkLabeled.png"
-import wikimedia from "./img/wikimedia.png"
-import osm from "./img/osm.png"
+import bingAerial from './img/bingAerial.png';
+import cartoBaseDarkLabeled from './img/cartoBaseDarkLabeled.png';
+import osm from './img/osm.png';
 
+// TO DO create updated thumbnail for wikimedia
 
 export default {
-    cartoDB_DarkMatter,
-    cartoDB_VoyagerNoLabels,
-    esri_WorldGrayCanvas,
-    esri_WorldImagery,
-    esri_WorldTopoMap,
-    noPreview,
+	bing_Aerial,
+	cartoDB_DarkMatter,
+	cartoDB_VoyagerNoLabels,
+	esri_WorldGrayCanvas,
+	esri_WorldImagery,
+	esri_WorldTopoMap,
+	noPreview,
+	openStreetMap_Mapnik,
+	wikimedia,
 
-    // to be deleted (void images)
-    bingAerial,
-    cartoBaseDarkLabeled,
-    wikimedia,
-    osm
+	// to be deleted (void images)
+	bingAerial,
+	cartoBaseDarkLabeled,
+	osm,
 };
