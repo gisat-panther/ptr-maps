a1e8c6bf726fa12df878f9bb031491128321b2ab
PavelVlach <vlach1989@gmail.com>
2022-10-06 11:05:09 +0200

    Deck.gl: vector layer multi-click select

---
+++ src/utils/feature.js
@@ -0,0 +1,56 @@
+import {stateManagement} from '@gisatcz/ptr-utils';
+
+/**
+ * Get feature key
+ * @param fidColumnName {string}
+ * @param feature {GeoJSONFeature}
+ * @returns {string}
+ */
+function getKey(fidColumnName, feature) {
+	return feature.id || feature.properties[fidColumnName];
+}
+
+/**
+ * Get selected feature keys (based on selection type)
+ * @param fidColumnName {string}
+ * @param feature {GeoJSONFeature}
+ * @param isMultiSelect {boolean}
+ * @param selectedFeatureKeys {Array}
+ * @return {Array} List of feature keys
+ */
+function getSelectedFeatureKeysOnClick(
+	fidColumnName,
+	feature,
+	isMultiSelect,
+	selectedFeatureKeys
+) {
+	const currentFeatureKey = getKey(fidColumnName, feature);
+
+	if (selectedFeatureKeys?.length) {
+		const alreadySelectedIndex = selectedFeatureKeys.indexOf(currentFeatureKey);
+
+		if (isMultiSelect) {
+			if (alreadySelectedIndex !== -1) {
+				return stateManagement.removeItemByIndex(
+					selectedFeatureKeys,
+					alreadySelectedIndex
+				);
+			} else {
+				return [...selectedFeatureKeys, currentFeatureKey];
+			}
+		} else {
+			if (alreadySelectedIndex !== -1) {
+				return [];
+			} else {
+				return [currentFeatureKey];
+			}
+		}
+	} else {
+		return [currentFeatureKey];
+	}
+}
+
+export default {
+	getKey,
+	getSelectedFeatureKeysOnClick,
+};
